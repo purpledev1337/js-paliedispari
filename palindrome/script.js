5f2a6109cf820c59defc89215ab6bfc019724a30
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-01 23:45:22 +0100

    palindrome with user input and print on html

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,13 +1,33 @@
 // Palidroma:
 // 1.  Chiedere all’utente di inserire una parola
-const userWord = prompt("Inserisci una parola");
-console.log("La parola scelta dall'utente è: ", userWord);
+// const userWord = prompt("Inserisci una parola");
+const inseredWord = document.querySelector("input");
+
+const clickBtn = document.getElementById("bottone");
+
+clickBtn.addEventListener("click",
+function() {
+    let userWord = inseredWord.value;
+    let isPalindrome = palindrome(userWord);
+    
+    console.log(`La parola scelta dall'utente è :"${userWord}"`);
+    document.querySelector(".lista_parole").innerHTML += (`<li>${userWord}</li>`);
+    // 3.2 Stampo un output se il risutato è vero e l'altro output se è falso
+    if (!isPalindrome) {
+        console.log(`La parola "${userWord}" non è palindroma!`);
+        document.querySelector(".parola").innerHTML = (`La parola "${userWord}" non è palindroma!`);
+    } else {
+        console.log(`La parola "${userWord}" è palindroma!`);
+        document.querySelector(".parola").innerHTML = (`La parola "${userWord}" è palindroma!`)
+    }
+}
+);
 
 // 2.  Creare una funzione per capire se la parola inserita è palindroma.
 function palindrome(wordToCheck) {
-        // 2.1 All'interno creo un ciclo for che pesca i caratteri della parola partendo dalla fine
+        // 2.1 All'interno creo un ciclo for che pesca i caratteri della parola fino ad arrivare alla metà della sua lunghezza
     for (var i = 0; i < wordToCheck.length / 2; i++) {
-        // 2.2 Se i caratteri presi in esame sono diversi ritorno un risultato falso
+        // 2.2 Metto a confronto il carattere dell'interazione con il rispettivo in fondo alla parola
         if (wordToCheck[i] !== wordToCheck[wordToCheck.length - 1 - i]) {
             return false;
         }
@@ -16,11 +36,3 @@ function palindrome(wordToCheck) {
 }
 // 3.  Dare l’output relativo.
 // 3.1 Assegno una variabile al risultato della funzione applicata alla parole dell'utente
-let isPalindrome = palindrome(userWord)
-
-// 3.2 Stampo un output se il risutato è vero e l'altro output se è falso
-if (!isPalindrome) {
-    console.log(`La parola ${userWord} non è palindroma!`);
-} else {
-    console.log(`La parola ${userWord} è palindroma!`);
-}
